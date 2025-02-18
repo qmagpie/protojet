87296ac6851930f5943f67d16a89c368d812acf8
Robert Paltik <robert.paltik@gmail.com>
2025-02-18 12:21:37 +0100

    signature_pad step 1, size management

--- src/app/signature/signature.page.ts
+++ src/app/signature/signature.page.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  ElementRef,
+  viewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
+  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
@@ -9,6 +15,7 @@ import {
   IonTitle,
   IonToolbar,
 } from '@ionic/angular/standalone';
+import SignaturePad from 'signature_pad';
 
 @Component({
   selector: 'app-signature',
@@ -16,6 +23,7 @@ import {
   styleUrls: ['./signature.page.scss'],
   standalone: true,
   imports: [
+    IonButton,
     IonButtons,
     IonContent,
     IonHeader,
@@ -26,8 +34,40 @@ import {
     FormsModule,
   ],
 })
-export class SignaturePage implements OnInit {
+export class SignaturePage implements AfterContentInit {
+  signatureCanvas =
+    viewChild.required<ElementRef<HTMLCanvasElement>>('signatureCanvas');
+  signaturePad!: SignaturePad;
+
   constructor() {}
 
-  ngOnInit() {}
+  // ionViewWillEnter() {
+  //   this.resizeCanvas();
+  // }
+
+  ngAfterContentInit(): void {
+    const canvasEl: HTMLCanvasElement = this.signatureCanvas().nativeElement;
+    this.signaturePad = new SignaturePad(canvasEl, {
+      backgroundColor: '#ffffff',
+      penColor: 'rgb(0, 0, 0)',
+    });
+  }
+
+  private resizeCanvas() {
+    const canvasEl: HTMLCanvasElement = this.signatureCanvas().nativeElement;
+    let ratio = Math.max(window.devicePixelRatio || 1, 1);
+    canvasEl.width = canvasEl.offsetWidth * ratio;
+    canvasEl.height = canvasEl.offsetHeight * ratio;
+    canvasEl.getContext('2d')!.scale(ratio, ratio);
+    this.signaturePad.clear(); // clear the pad on resize
+  }
+
+  clearSignature() {
+    this.signaturePad.clear();
+  }
+
+  saveSignature() {
+    const data = this.signaturePad.toDataURL();
+    console.log(data);
+  }
 }
