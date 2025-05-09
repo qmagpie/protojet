c36869763e5c9c963cea02bdf9edcc6a590f77a3
Robert Paltik <robert.paltik@gmail.com>
2025-05-09 11:37:58 +0200

    qr-scan - step 4 - scanner beep

--- src/app/scan/scan.page.ts
+++ src/app/scan/scan.page.ts
@@ -93,6 +93,7 @@ export class ScanPage implements OnInit {
   onScanSuccess(decodedText: any, decodedResult: any) {
     // Handle on success condition with the decoded text or result.
     console.log(`Scan result: ${decodedText}`, decodedResult);
+    this.beepScannerSound();
     // setTimeout(() => {
     this.scannedText = decodedText;
     this.scannedResult = decodedResult;
@@ -104,6 +105,7 @@ export class ScanPage implements OnInit {
   onScanSuccess2(decodedText: any, decodedResult: any) {
     // Handle on success condition with the decoded text or result.
     console.log(`Scan result: ${decodedText}`, decodedResult);
+    this.beepScannerSound();
     setTimeout(() => {
       this.scannedText = decodedText;
       this.scannedResult = decodedResult;
@@ -116,5 +118,25 @@ export class ScanPage implements OnInit {
     if (!errorMessage.includes('error = NotFoundException')) {
       console.error(`Scan error: ${errorMessage}`);
     }
+  }
+
+  beepScannerSound() {
+    const audioCtx = new AudioContext();
+
+    const oscillator = audioCtx.createOscillator();
+    const gainNode = audioCtx.createGain();
+
+    oscillator.type = 'square';
+    oscillator.frequency.setValueAtTime(1000, audioCtx.currentTime);
+
+    gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
+    gainNode.gain.linearRampToValueAtTime(0.5, audioCtx.currentTime + 0.01); // fade in
+    gainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + 0.1); // fade out
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+
+    oscillator.start();
+    oscillator.stop(audioCtx.currentTime + 0.15); // short beep
   }
 }
